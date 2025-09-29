00b8525bd7cf1a471efa0efb887224b9719a492c
John <johnlin8968@gmail.com>
2025-09-29 16:23:42 +1000

    added home page

---
+++ scenicshare-frontend/src/app/home-page/page.tsx
@@ -0,0 +1,28 @@
+import NavBar from "@/app/landing-page/components/NavBar";
+import Image from "next/image";
+import Header from "@/app/landing-page/components/Header";
+
+export default function HomePage() {
+  return (
+    <div className="relative min-h-screen">
+      <div className="fixed inset-0 w-full h-full z-0">
+        <Image
+          src="/scenic1.jpg"
+          alt="Scenic background"
+          fill
+          style={{
+            objectFit: "cover",
+            opacity: 0.4,
+            objectPosition: "center",
+          }}
+          priority
+          quality={100}
+        />
+      </div>
+      <main className="relative z-10 min-h-screen justify-center">
+        <NavBar />
+        <Header />
+      </main>
+    </div>
+  );
+}
